4340da3db6bab068bca5042136daf0581e61c608
Alex Adejumo <Teebillz07@gmail.com>
2020-02-13 23:01:49 +0100

    💫 Change the hover bg colour

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className='f3'>
-				{'This Magic Brain will detect faces in the pictures. Give it a try!'}
+				{'This Magic Brain will detect faces in the pictures. Paste your image URL and give it a try!'}
 			</p>
 			<div className='center'>
 				<div className=' form center pa4 br3 shadow-5'>
